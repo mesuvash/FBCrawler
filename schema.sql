0ab750bae54333156014ea7b81d3b2253e879aac
Suvash Sedhain <mesuvash@gmail.com>
2012-10-26 14:40:37 +1100

    Schema Edit

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 create table raw_crawled_data (
-    uid			     integer primary key not null,
-	basic_info	     text,
-    likes_info       text
+	uid				integer primary key not null,
+	basic_info		text,
+	likes_info		text
 );
