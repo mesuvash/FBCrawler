85764e7a67b5801f9ff03812b8089bff9b810146
Suvash Sedhain <mesuvash@gmail.com>
2012-10-27 00:02:39 +1100

    Docstring added

--- fetchUtils.py
+++ fetchUtils.py
@@ -3,9 +3,16 @@ import urllib
 import json
 
 def isError(response):
+    """
+    Facebook returns json with error key when error occurs
+    """
     return True if "error" in response else False
 
 def fb_call(call, args=None,no_processing=False):
+    """
+    Call graph API
+    """
+
     if not no_processing:
         url = "https://graph.facebook.com/{0}".format(call)
         params = urllib.urlencode(args)
@@ -15,10 +22,13 @@ def fb_call(call, args=None,no_processing=False):
     response = urllib.urlopen(request).read() 
     json_response = json.loads(response)
     if isError(json_response):
-        raise FBApiCallError
+        raise FBApiCallError( call + json_response )
     return json_response
 
 def get_fb_likes(uid,access_token,limit=1000):
+    """
+    Call graph api to get user's likes information
+    """
     def get_fb_likes_recursively(response):
         if "next" in response["paging"]:
             _response = fb_call(response["paging"]["next"],
@@ -32,4 +42,7 @@ def get_fb_likes(uid,access_token,limit=1000):
     return get_fb_likes_recursively(response)
 
 def get_fb_basic_info(uid,access_token):
+    """
+    Call graph api to get user's basic information
+    """
     return fb_call(uid ,args={"access_token":access_token})
